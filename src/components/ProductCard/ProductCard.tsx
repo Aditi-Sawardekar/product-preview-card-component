a38f87f25507f1af030f3534be5a02c411add431
Aditi-Sawardekar <aditi.sawardekar@gmail.com>
2025-08-19 13:10:02 +0200

    Update ProductCard to integrate all individual card components

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,3 +1,26 @@
+import CardButton from "../CardButton/CardButton";
+import CardCategory from "../CardCategory/CardCategory";
+import CardDescription from "../CardDescription/CardDescription";
+import CardImage from "../CardImage/CardImage";
+import CardPrice from "../CardPrice/CardPrice";
+import CardTitle from "../CardTitle/CardTitle";
+
 export default function ProductCard() {
-  return <h2>Product Card</h2>;
+  return (
+    <>
+      <h2>Product Card</h2>
+      <CardImage
+        src="src\assets\images\image-product-mobile.jpg"
+        alt="Image of Gabrielle Essence Eau De Parfum"
+      />
+      <CardCategory category="perfume" />
+      <CardTitle title="Gabrielle Essence Eau De Parfum" />
+      <CardDescription
+        description="A floral, solar and voluptuous interpretation composed by Olivier Polge,
+      perfumer-creator for the house of Chanel."
+      />
+      <CardPrice currency="$" discountedPrice={149.99} originalPrice={169.99} />
+      <CardButton label="Add to Cart" />
+    </>
+  );
 }
